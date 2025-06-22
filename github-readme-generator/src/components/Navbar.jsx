c904fe8485eafa209be541f8e39e822dd4a95fb5
Abhijeet Bhale <abhijeetbhale7@gmail.com>
2025-06-22 13:46:23 +0530

    Change in the Website name from the Navbar

--- github-readme-generator/src/components/Navbar.jsx
+++ github-readme-generator/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-blue-800 text-white p-4 shadow-md flex justify-between items-center" id='nav-bar'>
-      <h1 className="text-2xl font-bold" id='nav'>GitHub Readme Generator</h1>
+      <h1 className="text-2xl font-bold" id='nav'>GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-markdown-fill" viewBox="0 0 16 16">
+        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.5 1a.5.5 0 0 0-.5.5v3.793L9.854 8.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L12 9.293V5.5a.5.5 0 0 0-.5-.5M3.56 7.01h.056l1.428 3.239h.774l1.42-3.24h.056V11h1.073V5.001h-1.2l-1.71 3.894h-.039l-1.71-3.894H2.5V11h1.06z" />
+      </svg></span> Generator</h1>
       <div className="flex gap-4 items-center" id='git-btns'>
         <a
           href="https://github.com/abhijeetBhale/Readme-Generator"
@@ -11,7 +13,7 @@ const Navbar = () => {
           rel="noopener noreferrer"
           className="flex items-center gap-2 bg-white text-gray-800 px-4 py-2 rounded-full shadow-md hover:bg-gray-100 hover:scale-105 transition-all duration-200"
         >
-          <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" id='star'><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span>
+          <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" id='star'><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg></span>
         </a>
         <a
           href="https://github.com/abhijeetBhale/github-readme-generator/fork"
