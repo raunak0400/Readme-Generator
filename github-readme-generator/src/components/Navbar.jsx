21db3996f25bf5688249a48412d74b6df3c4ca49
Abhijeet Bhale <abhijeetbhale7@gmail.com>
2025-07-04 00:30:25 +0530

    Addition of the Demo Video Button

--- github-readme-generator/src/components/Navbar.jsx
+++ github-readme-generator/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './GitHubStar';
+import DemoVideo from './DemoVideo';
 
 const Navbar = () => {
   return (
@@ -8,6 +9,7 @@ const Navbar = () => {
         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.5 1a.5.5 0 0 0-.5.5v3.793L9.854 8.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L12 9.293V5.5a.5.5 0 0 0-.5-.5M3.56 7.01h.056l1.428 3.239h.774l1.42-3.24h.056V11h1.073V5.001h-1.2l-1.71 3.894h-.039l-1.71-3.894H2.5V11h1.06z" />
       </svg></span> Generator</h1>
       <div className="flex gap-4 items-center" id='git-btns'>
+        <DemoVideo />
         <a
           href="https://github.com/abhijeetBhale/Readme-Generator"
           target="_blank"
